3fe7c24364af686147ba402c20e12e96a813228e
Milan Oli <milan2003oli@gmail.com>
2026-05-30 14:06:29 +0530

    message route complete will need to do socket.io integration later

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 export const getUsersForSidebar = async (req,res)=>{
     try {
         const loggedInUserId = req.user._id;
@@ -9,5 +10,48 @@ export const getUsersForSidebar = async (req,res)=>{
        console.log("Error in getUsersForSidebar controller",error.message);
        return res.status(500).json({message: "Internal server error"});
 
+    }
+}
+
+export const getMessages = async (req,res)=>{
+    try {
+       const {id:userToChatId} = req.params;
+       const myId = req.user._id;
+       const messages = await Message.find({
+        $or:[
+        {senderId:myId, receiverId: userToChatId},
+        {senderId: userToChatId, receiverId: myId}
+       ]});
+       return res.status(200).json(messages);
+    } catch (error) {
+       console.log("Error in getMessages controller",error.message);
+       return res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const sendMessage = async (req,res)=>{
+    try {
+        const {text, image} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+        await newMessage.save();
+
+        return res.status(201).json(newMessage);
+    } catch (error) {
+        console.log('Error in sendMessage controller',error.message);
+        return res.status(500).json({message: "Internal server error"});
     }
 }
