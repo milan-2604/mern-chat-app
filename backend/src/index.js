688842862d6f809d47e336b20d051c8a32746a4f
Milan Oli <milan2003oli@gmail.com>
2026-05-30 15:43:41 +0530

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
@@ -12,11 +13,12 @@ app.get('/', (req, res) => {
 });
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use('/api/auth',authRoutes);
-app.use('/api/message',messageRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
-app.listen(port,()=>{
-    console.log("Server running at port",port);
+app.listen(port, () => {
+    console.log("Server running at port", port);
     connectDB();
 })
