e4011d90a83b6f71dc3888bfd656d5d016d601fe
Milan Oli <milan2003oli@gmail.com>
2026-05-30 13:11:04 +0530

    message route initiallized with get users functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser'
-import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 dotenv.config();
 const app = express();
@@ -12,7 +13,8 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes);
 
 app.listen(port,()=>{
     console.log("Server running at port",port);
