accc861c9eb6e9a6e5e63cd237a32422e584e63b
Milan Oli <milan2003oli@gmail.com>
2026-05-30 12:48:50 +0530

    Authorization completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req,res)=>{
    try {
     const {fullName,email,password} = req.body; 
@@ -66,5 +67,23 @@ export const logout = (req,res)=>{
     }
 }
 export const updateProfile = async (req,res)=>{
-    
+    try {
+       const {profilePic} = req.body;
+       const userId = req.user._id;
+       if(!profilePic)return res.status(400).json({message: "Profile pic is required"});
+       const uploadResponse = await cloudinary.uploader.upload(profilePic);
+       const updatedUser = await User.findByIdAndUpdate(userId,{profilePic: uploadResponse.secure_url}, {new:true});
+       return res.status(200).json(updatedUser);
+    } catch (error) {
+       console.log("Error in updateProfile controller",error.message);
+       return res.status(500).json({message: "Interanl server error"});
+    }
+}
+export const checkAuth = async (req,res)=>{
+    try {
+       return res.status(200).json(req.user);
+    } catch (error) {
+       console.log("Error in checkAuth controller",error.message);
+       return res.status(500).json({message: "Internal server error"});
+    }
 }
