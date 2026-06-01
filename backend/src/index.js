c98052c5619796778fb29f835e3638289e639d44
Milan Oli <milan2003oli@gmail.com>
2026-06-01 12:35:56 +0530

    fixed messageController and added limit image uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,14 +11,15 @@ const port = process.env.PORT || 5001;
 app.get('/', (req, res) => {
     res.send("Welcome to Chat API");
 });
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); 
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(port, () => {
     console.log("Server running at port", port);
     connectDB();
-})
+});
