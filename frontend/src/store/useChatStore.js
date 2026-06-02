4902f5d09efaeebe8d7b3bba2b3064a54591c7c9
Milan Oli <milan2003oli@gmail.com>
2026-06-02 09:47:13 +0530

    online toggle functionality added

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -53,7 +53,11 @@ export const useChatStore = create((set,get)=>({
         if(!selectedUser)return;
 
         const socket = useAuthStore.getState().socket;
+
+
+
         socket.on("newMessage",(newMessage)=>{
+            if(newMessage.senderId !== selectedUser._id)return;
             set({
                 messages: [...get().messages,newMessage],
             });
