a68699ceaaecf19e18223e2f0d6bf44ebb34a1e8
Milan Oli <milan2003oli@gmail.com>
2026-06-02 10:22:01 +0530

    ready for deployment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import { data } from "react-router-dom";
 import toast from "react-hot-toast";
 import {io} from 'socket.io-client';
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; 
 export const useAuthStore = create((set,get)=>({
     authUser: null,
     isSigningUp: false,
